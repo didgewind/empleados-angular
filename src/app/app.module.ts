26ace230f7fda557de11c9284429586b754a84fe
made <kike@mazarredos.com>
2018-05-18 11:28:52 +0100

    Editar datos del empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
@@ -10,7 +11,8 @@ import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empl
     ListaEmpleadosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
